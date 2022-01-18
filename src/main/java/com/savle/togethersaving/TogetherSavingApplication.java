177a69ace4fd1c365e17be1b82ab4b9fbc610b1b
Jeongisu94 <mustdoit2020@gmail.com>
2022-01-18 15:42:01 +0900

    Feat: create user entitiy

--- src/main/java/com/savle/togethersaving/TogetherSavingApplication.java
+++ src/main/java/com/savle/togethersaving/TogetherSavingApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TogetherSavingApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(TogetherSavingApplication.class, args);
     }
-
 }
