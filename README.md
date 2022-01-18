c852cc2de8ab3a2008f468ea7aeffd7e13f1c29f
Jeongisu94 <mustdoit2020@gmail.com>
2022-01-18 13:49:48 +0900

    Init: first initialize project

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # together-saving-BE
+
 함께 저축하기 서비스 BE 개발 레포입니다.
---
+++ src/main/java/com/savle/togethersaving/TogetherSavingApplication.java
@@ -0,0 +1,13 @@
+package com.savle.togethersaving;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TogetherSavingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TogetherSavingApplication.class, args);
+    }
+
+}
