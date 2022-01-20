f6da29aeb58ae99adf6606bc05ac9f6bf53d110f
yangdongjue5510 <djwhy5510@gmail.com>
2022-01-20 22:17:24 +0900

    feat: mapping ChallengeUser

--- src/main/java/com/savle/togethersaving/entity/ChallengeUser.java
+++ src/main/java/com/savle/togethersaving/entity/ChallengeUser.java
@@ -3,8 +3,15 @@ package com.savle.togethersaving.entity;
 import lombok.*;
 
 import javax.persistence.Column;
+import javax.persistence.ConstraintMode;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.ForeignKey;
 import javax.persistence.Id;
+import javax.persistence.IdClass;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
 import java.io.Serializable;
 
 @AllArgsConstructor
@@ -13,6 +20,7 @@ import java.io.Serializable;
 @Entity
 @Setter
 @Getter
+@IdClass(ChallengeTagPK.class)
 public class ChallengeUser {
     @Id
     private Long challengeId;
@@ -24,4 +32,11 @@ public class ChallengeUser {
 
     private Boolean isAutomated; // 자동 저축 여부
 
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(nullable = false, insertable = false, foreignKey = @ForeignKey(ConstraintMode.CONSTRAINT))
+    private Challenge challenge;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(nullable = false, insertable = false, foreignKey = @ForeignKey(ConstraintMode.CONSTRAINT))
+    private User user;
 }
