13107c79818ca4eb1879ca3dd92c61d02fdab300
yangdongjue5510 <djwhy5510@gmail.com>
2022-01-20 19:34:04 +0900

    fix: conflict resolve in Challenge

--- src/main/java/com/savle/togethersaving/entity/Challenge.java
+++ src/main/java/com/savle/togethersaving/entity/Challenge.java
@@ -2,13 +2,22 @@ package com.savle.togethersaving.entity;
 
 import lombok.*;
 
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
 import com.sun.istack.NotNull;
+
 
 @Builder
 @NoArgsConstructor
@@ -21,7 +30,6 @@ public class Challenge {
     @GeneratedValue
     @Id
     private Long challengeId;
-
 
     @ManyToOne
     @JoinColumn(name = "host_id")
