687d4a7508cbd17481a81ff8fdd33e0663ec4cf7
yangdongjue5510 <djwhy5510@gmail.com>
2022-01-20 21:03:34 +0900

    fix: resolving conflict ChallengeUser

--- src/main/java/com/savle/togethersaving/entity/ChallengeUser.java
+++ src/main/java/com/savle/togethersaving/entity/ChallengeUser.java
@@ -13,18 +13,16 @@ import java.io.Serializable;
 @Entity
 @Setter
 @Getter
-public class ChallengeUser implements Serializable {
+
+public class ChallengeUser {
     @Id
     private Long challengeId;
 
     @Id
-    @Column
     private Long userId;
 
-    @Column
     private Long accumulatedBalance;
 
-    @Column
     private Boolean isAutomated; // 자동 저축 여부
 
 }
