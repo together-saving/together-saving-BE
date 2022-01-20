3c7b37a1ddd0b06142bd97a9294e1bb01d55106d
yangdongjue5510 <djwhy5510@gmail.com>
2022-01-20 19:41:39 +0900

    feat: mapping ChallengeTag

--- src/main/java/com/savle/togethersaving/entity/ChallengeTag.java
+++ src/main/java/com/savle/togethersaving/entity/ChallengeTag.java
@@ -2,8 +2,14 @@ package com.savle.togethersaving.entity;
 
 import lombok.*;
 
+import javax.persistence.ConstraintMode;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.ForeignKey;
 import javax.persistence.Id;
+import javax.persistence.IdClass;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 
 @Builder
 @NoArgsConstructor
@@ -11,6 +17,7 @@ import javax.persistence.Id;
 @Setter
 @Getter
 @Entity
+@IdClass(ChallengeTagPK.class)
 public class ChallengeTag {
 
     @Id
@@ -18,4 +25,12 @@ public class ChallengeTag {
 
     @Id
     private String name;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(nullable = false, insertable = false, foreignKey = @ForeignKey(ConstraintMode.CONSTRAINT))
+    private Challenge challenge;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(nullable = false, insertable = false, foreignKey = @ForeignKey(ConstraintMode.CONSTRAINT))
+    private Tag tag;
 }
