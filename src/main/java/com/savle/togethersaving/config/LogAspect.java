2b17abe54a3a520dd73eb3cbc87b2a3652a59a7a
yangdongjue5510 <djwhy5510@gmail.com>
2022-01-21 23:32:34 +0900

    feat: method args, spent time, request uri logging

--- src/main/java/com/savle/togethersaving/config/LogAspect.java
+++ src/main/java/com/savle/togethersaving/config/LogAspect.java
@@ -4,12 +4,14 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
-import org.aspectj.lang.Signature;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StopWatch;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -22,9 +24,42 @@ public class LogAspect {
 	@Pointcut("execution(* com.savle.togethersaving..*.*(..))")
 	public void allMethodCut() {}
 
-	@Before("allMethodCut()")
-	public void beforeMethodLog(JoinPoint joinPoint) {
-		Signature signature = joinPoint.getSignature();
-		log.info("======== METHOD CALL : {} ========", signature.toShortString());
+	@Pointcut("execution(* com.savle.togethersaving.controller..*.*(..))")
+	public void allControllerCut() {}
+
+	@Before("allControllerCut()")
+	public void beforeControllerMethodLog(JoinPoint joinPoint) {
+		HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder
+			.currentRequestAttributes()).getRequest();
+		log.info("-------- REQUEST URI : {} --------", request.getRequestURI());
+		logArgsArray(joinPoint.getArgs());
+	}
+
+	@Before(value = "allMethodCut()")
+	public void beforeRepositoryMethodLog(JoinPoint joinPoint) {
+		log.info("======== SIGNATURE CALLED : {} ========", joinPoint.getSignature().toShortString());
+		logArgsArray(joinPoint.getArgs());
+	}
+
+	@Around("allMethodCut()")
+	public Object aroundMethodLog(ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
+		StopWatch watch = new StopWatch();
+		watch.start();
+		Object result = proceedingJoinPoint.proceed();
+		watch.stop();
+		String signature = proceedingJoinPoint.getSignature().toShortString();
+		log.info("======== {} TIME SPENT : {} seconds! ========", signature, watch.getTotalTimeSeconds());
+		return result;
+	}
+
+	private void logArgsArray(Object[] args) {
+		if (args.length == 0) {
+			return;
+		}
+		log.info("======== ARGUMENTS ========");
+		for (Object arg : args) {
+			log.info("{} {}", arg.getClass().getSimpleName(), arg);
+		}
+		log.info("===========================");
 	}
 }
--- src/main/java/com/savle/togethersaving/controller/AOPTestController.java
+++ src/main/java/com/savle/togethersaving/controller/AOPTestController.java
@@ -1,13 +1,20 @@
-package com.savle.togethersaving;
+package com.savle.togethersaving.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.savle.togethersaving.service.AOPTestService;
 
 @RestController
 public class AOPTestController {
 
+	@Autowired
+	AOPTestService service;
+
 	@GetMapping("/")
 	public String methodNameTest() {
+		service.aopService(1);
 		return "Response";
 	}
 }
