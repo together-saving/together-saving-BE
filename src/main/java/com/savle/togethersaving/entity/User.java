a986c17f5af2f7d10fb63bfbbf782c9f15067fe8
yangdongjue5510 <djwhy5510@gmail.com>
2022-01-21 15:52:09 +0900

    fix: User mapping

--- src/main/java/com/savle/togethersaving/entity/User.java
+++ src/main/java/com/savle/togethersaving/entity/User.java
@@ -1,9 +1,13 @@
 package com.savle.togethersaving.entity;
 
+import com.sun.istack.NotNull;
 import lombok.*;
 import org.hibernate.annotations.ColumnDefault;
 
 import javax.persistence.*;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 @AllArgsConstructor
 @NoArgsConstructor
@@ -12,25 +16,47 @@ import javax.persistence.*;
 @Setter
 @Getter
 public class User {
+
     @Id
     @GeneratedValue
     private Long userId;
 
+    @NotNull
     private String email;
 
-    private String birth;
+    @NotNull
+    private LocalDate birth;
 
+    @NotNull
     private Boolean gender;
 
+    @NotNull
     private String phoneNumber;
+
 
     private String profilePicture;
 
+
+    @ColumnDefault("닉네임을 설정해주세요.")
     private String nickName;
 
-    private Boolean isAdmin;
+    @NotNull
+    @Enumerated(EnumType.STRING)
+    @ColumnDefault("`USER`")
+    private Role role;
 
+    @ColumnDefault("0")
     private Long point;
 
+    @ColumnDefault("0")
     private Long reward;
+
+    @OneToMany(mappedBy = "owner")
+    private List<Account> accountList = new ArrayList<>();
+
+    @OneToMany(mappedBy = "hopingPerson")
+    private List<Wish> wishedChallengesList = new ArrayList<>();
+
+    @OneToMany(mappedBy = "reviewer")
+    private List<ChallengeReview> reviewList = new ArrayList<>();
 }
