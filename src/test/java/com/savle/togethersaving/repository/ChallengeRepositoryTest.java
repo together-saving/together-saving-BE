4f29de716a4feec2bae18a099256a9f91b0923dd
yangdongjue5510 <djwhy5510@gmail.com>
2022-01-22 09:05:42 +0900

    fix: ChallengeRepositoryTest

--- src/test/java/com/savle/togethersaving/repository/ChallengeRepositoryTest.java
+++ src/test/java/com/savle/togethersaving/repository/ChallengeRepositoryTest.java
@@ -6,54 +6,61 @@ import java.util.List;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.data.domain.Sort;
 
 import com.savle.togethersaving.entity.Challenge;
-import com.savle.togethersaving.entity.Mode;
 
-@SpringBootTest
+@DataJpaTest
 class ChallengeRepositoryTest {
 
-	private final ChallengeRepository challengeRepository;
-
 	@Autowired
-	ChallengeRepositoryTest(ChallengeRepository challengeRepository) {
-		this.challengeRepository = challengeRepository;
-	}
+	private ChallengeRepository challengeRepository;
 
 	@Test
 	void beforeStartChallengeTest() {
-		challengesSave();
+		//given
+		Challenge previous = Challenge.builder()
+			.title("1")
+			.startDate(LocalDate.now().plusDays(1)).build();
+		Challenge after = Challenge.builder()
+			.title("2")
+			.startDate(LocalDate.now().minusDays(1)).build();
+		challengeRepository.save(previous);
+		challengeRepository.save(after);
+
+		//when
 		List<Challenge> challengesByStartDateAfter
 			= challengeRepository.findChallengesByStartDateGreaterThan(LocalDate.now());
-		Assertions.assertThat(challengesByStartDateAfter.size()).isEqualTo(2);
-		challengeRepository.deleteAll();
+
+		//then
+		Assertions.assertThat(challengesByStartDateAfter.size()).isEqualTo(1);
 	}
 
 	@Test
 	void popularChallengeTest() {
-		challengesSave();
-		List<Challenge> popularChallenges = challengeRepository.findChallengesByStartDateGreaterThan(LocalDate.now(),
-			Sort.by("members").descending());
-		Assertions.assertThat(popularChallenges.get(0).getMembers()).isEqualTo(3L);
-		challengeRepository.deleteAll();
+		//given
+		Challenge previous = Challenge.builder()
+			.title("1")
+			.members(1L)
+			.startDate(LocalDate.now().plusDays(1)).build();
+		Challenge after = Challenge.builder()
+			.title("2")
+			.members(2L)
+			.startDate(LocalDate.now().minusDays(1)).build();
+		Challenge biggest = Challenge.builder()
+			.title("3")
+			.members(15L)
+			.startDate(LocalDate.now().plusDays(1)).build();
+		challengeRepository.save(previous);
+		challengeRepository.save(after);
+		challengeRepository.save(biggest);
+
+		//when
+		List<Challenge> popularChallenges
+			= challengeRepository
+				.findChallengesByStartDateGreaterThan(LocalDate.now(), Sort.by("members").descending());
+		//then
+		Assertions.assertThat(popularChallenges.get(0).getMembers()).isEqualTo(15L);
 	}
-
-	private void challengesSave() {
-		Challenge chal1 = Challenge.builder().content("previous").entryFee(0L).members(2L)
-			.mode(Mode.FREE).payment(0L).startDate(LocalDate.now().minusDays(1L))
-			.period(1).build();
-		Challenge chal2 = Challenge.builder().content("post").entryFee(0L).members(1L)
-			.mode(Mode.FREE).payment(0L).startDate(LocalDate.now().plusDays(1L))
-			.period(1).build();
-		Challenge chal3 = Challenge.builder().content("post").entryFee(0L).members(3L)
-			.mode(Mode.FREE).payment(0L).startDate(LocalDate.now().plusDays(1L))
-			.period(1).build();
-		challengeRepository.save(chal1);
-		challengeRepository.save(chal2);
-		challengeRepository.save(chal3);
-	}
-
-
 }
