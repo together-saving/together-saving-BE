11aaf6ebded5fb418ff252bd0446c700f57b3c50
SENUNGHOONYANG <sheepsh93@gmail.com>
2022-01-23 21:15:29 +0900

    feat : Create dto related to review entity

---
+++ src/main/java/com/savle/togethersaving/dto/review/ReviewCreateDto.java
@@ -0,0 +1,26 @@
+package com.savle.togethersaving.dto.review;
+
+
+import com.savle.togethersaving.entity.Challenge;
+import com.savle.togethersaving.entity.ChallengeReview;
+import com.savle.togethersaving.entity.User;
+import lombok.*;
+
+@Builder
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class ReviewCreateDto {
+
+    private Long challengeId;
+    private String reviewContent;
+
+    public ChallengeReview toEntity(User user, Challenge challenge){
+        return ChallengeReview
+                .builder()
+                .content(this.reviewContent)
+                .challenge(challenge)
+                .reviewer(user)
+                .build();
+    }
+}
