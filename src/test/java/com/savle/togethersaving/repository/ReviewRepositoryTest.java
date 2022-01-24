8d37f63f87839b42fc0303f1e657b06944d1a203
SENUNGHOONYANG <sheepsh93@gmail.com>
2022-01-24 12:42:46 +0900

    test : Implemented review controller,service,repository tests

--- src/test/java/com/savle/togethersaving/repository/ReviewRepositoryTest.java
+++ src/test/java/com/savle/togethersaving/repository/ReviewRepositoryTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
+import javax.persistence.EntityManager;
 import java.time.LocalDate;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -23,43 +24,26 @@ class ReviewRepositoryTest {
     @Autowired
     UserRepository userRepository;
 
-    protected final User user = User.builder()
-            .email("sheep@naver.com")
-            .birth(LocalDate.of(2020, 01, 01))
-            .gender(true)
-            .phoneNumber("010-1234-5678")
-            .profilePicture("http://asdasd.com")
-            .nickname("NICK")
-            .role(Role.USER)
-            .point(0L)
-            .reward(0L)
-            .build();
+    @Autowired
+    EntityManager em;
 
-    protected final Challenge challenge = Challenge.builder()
-            .host(user)
-            .startDate(LocalDate.now().plusDays(2L))
-            .title("돈 모으자")
-            .content("이 챌린지는 담배를 아껴서 돈 모으는 챌린지다")
-            .payment(5000L)
-            .members(100L)
-            .mode(Mode.FREE)
-            .entryFee(5000L)
-            .period(3)
-            .thumbnail("http://qweqweqwe.com")
-            .build();
-
-    protected final ChallengeReview challengeReview = ChallengeReview.builder()
-            .reviewer(user)
-            .challenge(challenge)
-            .content("이거 재미있네요")
-            .build();
+    protected User user;
+    protected Challenge challenge;
 
     @DisplayName("Review Save 성공.")
     @Test
     void reviewSaveTest() {
         createUserAndChallenge();
+        em.flush();
+        ChallengeReview challengeReview = ChallengeReview.builder()
+                .reviewer(user)
+                .challenge(challenge)
+                .content("이거 재미있네요")
+                .build();
 
-        ChallengeReview savedChallengeReview = reviewRepository.save(challengeReview);
+        reviewRepository.save(challengeReview);
+        ChallengeReview savedChallengeReview = reviewRepository.findById(1L).orElseThrow();
+
 
         assertThat(savedChallengeReview.getReviewId()).isEqualTo(1L);
         assertThat(savedChallengeReview.getChallenge().getChallengeId()).isEqualTo(1L);
@@ -70,8 +54,35 @@ class ReviewRepositoryTest {
 
     protected void createUserAndChallenge() {
 
+        user = User.builder()
+                .email("sheep@naver.com")
+                .birth(LocalDate.of(2020, 01, 01))
+                .gender(true)
+                .phoneNumber("010-1234-5678")
+                .profilePicture("http://asdasd.com")
+                .nickname("NICK")
+                .role(Role.USER)
+                .point(0L)
+                .reward(0L)
+                .build();
+
+
+        challenge = Challenge.builder()
+                .host(user)
+                .startDate(LocalDate.now().plusDays(2L))
+                .title("돈 모으자")
+                .content("이 챌린지는 담배를 아껴서 돈 모으는 챌린지다")
+                .payment(5000L)
+                .members(100L)
+                .mode(Mode.FREE)
+                .entryFee(5000L)
+                .period(3)
+                .thumbnail("http://qweqweqwe.com")
+                .build();
+
         userRepository.save(user);
         challengeRepository.save(challenge);
+
     }
 
 }
--- src/test/java/com/savle/togethersaving/service/ReviewServiceTest.java
+++ src/test/java/com/savle/togethersaving/service/ReviewServiceTest.java
@@ -1,24 +1,93 @@
 package com.savle.togethersaving.service;
 
+import com.savle.togethersaving.dto.review.ReviewCreateDto;
 import com.savle.togethersaving.dto.review.ReviewDto;
+import com.savle.togethersaving.entity.*;
+import com.savle.togethersaving.repository.ReviewRepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.time.LocalDate;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.doReturn;
+
+@ExtendWith(MockitoExtension.class)
+class ReviewServiceTest {
+
+    @Mock
+    protected UserService userService;
+    @Mock
+    protected ChallengeService challengeService;
+    @Mock
+    private ReviewRepository reviewRepository;
+
+    @InjectMocks
+    private ReviewService reviewService;
 
 
-class ReviewServiceTest extends TestUtil {
+    protected final User user = User.builder()
+            .userId(1L)
+            .email("sheep@naver.com")
+            .birth(LocalDate.of(2020, 01, 01))
+            .gender(true)
+            .phoneNumber("010-1234-5678")
+            .profilePicture("http://asdasd.com")
+            .nickname("NICK")
+            .role(Role.USER)
+            .point(0L)
+            .reward(0L)
+            .build();
+
+    protected final Challenge challenge = Challenge.builder()
+            .challengeId(1L)
+            .host(user)
+            .startDate(LocalDate.now().plusDays(2L))
+            .title("돈 모으자")
+            .content("이 챌린지는 담배를 아껴서 돈 모으는 챌린지다")
+            .payment(5000L)
+            .members(100L)
+            .mode(Mode.FREE)
+            .entryFee(5000L)
+            .period(3)
+            .thumbnail("http://qweqweqwe.com")
+            .build();
+
+
+    protected final ReviewCreateDto reviewCreateDto = ReviewCreateDto.builder()
+            .userId(1L)
+            .challengeId(1L)
+            .reviewContent("즐겁네요")
+            .build();
 
 
     @DisplayName("SaveReview() -리뷰 저장 성공")
     @Test
     void shouldSavedReviewSuccessfully() {
 
-        //given
-        createUserAndChallenge();
 
-        //when
-        ReviewDto savedReviewDto = reviewService.saveReview(user.getUserId(), reviewCreateDto);
+        doReturn(user)
+                .when(userService).getUserByUserId(reviewCreateDto.getUserId());
+
+        doReturn(challenge)
+                .when(challengeService).getChallengeByChallengeId(reviewCreateDto.getChallengeId());
+
+        given(reviewRepository.save(any(ChallengeReview.class))).will(invocation -> ChallengeReview.builder()
+                .reviewId(1L)
+                .reviewer(user)
+                .challenge(challenge)
+                .content(reviewCreateDto.getReviewContent())
+                .build());
+
+
+        ReviewDto savedReviewDto = reviewService.saveReview(reviewCreateDto);
+
 
         assertEquals(savedReviewDto.getChallengeId(), 1L);
         assertEquals(savedReviewDto.getReviewId(), 1L);
@@ -26,5 +95,6 @@ class ReviewServiceTest extends TestUtil {
         assertEquals(savedReviewDto.getContent(), "즐겁네요");
 
     }
+
 
 }
