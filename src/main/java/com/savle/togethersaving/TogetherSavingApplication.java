01877ba8620f9fb2560727a9a74f6731b03d7875
SENUNGHOONYANG <sheepsh93@gmail.com>
2022-01-24 12:43:31 +0900

    refactor : refactoring JpaAuditing function

--- src/main/java/com/savle/togethersaving/TogetherSavingApplication.java
+++ src/main/java/com/savle/togethersaving/TogetherSavingApplication.java
@@ -2,9 +2,7 @@ package com.savle.togethersaving;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@EnableJpaAuditing
 @SpringBootApplication
 public class TogetherSavingApplication {
     public static void main(String[] args) {
