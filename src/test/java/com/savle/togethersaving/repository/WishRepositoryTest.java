6cd3cbd14ec4744427561e2edecedb04823ad26f
Jeongisu94 <mustdoit2020@gmail.com>
2022-01-25 14:19:23 +0900

    fix : existsByHopingPerson_UserIdAndChallenge()

--- src/test/java/com/savle/togethersaving/repository/WishRepositoryTest.java
+++ src/test/java/com/savle/togethersaving/repository/WishRepositoryTest.java
@@ -4,15 +4,13 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import java.time.LocalDate;
 
+import com.savle.togethersaving.entity.*;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
-import com.savle.togethersaving.entity.Challenge;
-import com.savle.togethersaving.entity.User;
-import com.savle.togethersaving.entity.Wish;
 import com.savle.togethersaving.service.WishService;
 
 @DataJpaTest
@@ -31,11 +29,33 @@ class WishRepositoryTest {
 	@Test
 	void existsByHopingPerson_UserIdAndChallenge() {
 		//when
-		Challenge previous = Challenge.builder()
-			.title("1")
-			.startDate(LocalDate.now().plusDays(1)).build();
-		User user = User.builder().email("1").birth(LocalDate.now()).gender(true).password("1234").phoneNumber("123").build();
+
+		User user = User.builder()
+				.email("sheep@naver.com")
+				.birth(LocalDate.of(2020, 01, 01))
+				.gender(true)
+				.password("1234")
+				.phoneNumber("010-1234-5678")
+				.profilePicture("http://asdasd.com")
+				.nickname("NICK")
+				.role(Role.USER)
+				.point(0L)
+				.reward(0L)
+				.build();
 		userRepository.save(user);
+
+		Challenge previous = Challenge.builder()
+				.host(user)
+				.startDate(LocalDate.now().plusDays(2L))
+				.title("돈 모으자")
+				.content("이 챌린지는 담배를 아껴서 돈 모으는 챌린지다")
+				.payment(5000L)
+				.members(100L)
+				.mode(Mode.FREE)
+				.entryFee(5000L)
+				.period(3)
+				.thumbnail("http://qweqweqwe.com")
+				.build();
 		challengeRepository.save(previous);
 
 		Wish wish = Wish.builder().challenge(previous).hopingPerson(user).build();
