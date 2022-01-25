89e1e8436adc088f8aa94736f7d4cefc1b5f72e3
yangdongjue5510 <djwhy5510@gmail.com>
2022-01-25 21:02:44 +0900

    feat: 위시 컨트롤러 삭제 기능 구현

--- src/main/java/com/savle/togethersaving/controller/WishController.java
+++ src/main/java/com/savle/togethersaving/controller/WishController.java
@@ -2,6 +2,7 @@ package com.savle.togethersaving.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
@@ -21,9 +22,16 @@ public class WishController {
 	private final WishService wishService;
 
 	@PostMapping("/users/challenges/{challengeId}/wish")
-	public ResponseEntity<Data<String>> addWish(@PathVariable Long challengeId,
+	public ResponseEntity<HttpStatus> addWish(@PathVariable Long challengeId,
 		@RequestHeader("user-id") Long userId) {
 		wishService.addWish(userId, challengeId);
+		return new ResponseEntity<>(HttpStatus.OK);
+	}
+
+	@DeleteMapping("/users/challenges/{challengeId}/wish")
+	public ResponseEntity<HttpStatus> deleteWish(@PathVariable Long challengeId,
+		@RequestHeader("user-id") Long userId) {
+		wishService.deleteWish(userId, challengeId);
 		return new ResponseEntity<>(HttpStatus.OK);
 	}
 }
