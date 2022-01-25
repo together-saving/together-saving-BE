721193b2c42a512dfb4afadbb8df85fd492844c0
SENUNGHOONYANG <sheepsh93@gmail.com>
2022-01-26 00:54:38 +0900

    test : retrieveParticipatingChallenge repository test

---
+++ src/test/java/com/savle/togethersaving/repository/ChallengeUserRepositoryTest.java
@@ -0,0 +1,29 @@
+package com.savle.togethersaving.repository;
+
+import com.savle.togethersaving.entity.ChallengeUser;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+
+import java.util.List;
+
+public class ChallengeUserRepositoryTest extends RepositoryTestUtil {
+
+    @Test
+    @DisplayName("참여중인 챌린지 조회 테스트")
+    void beforeStartChallengeTest() {
+        //given
+        createUserAndChallengeSaved();
+        createChallengeUser();
+        //when
+
+        List<ChallengeUser> participatingChallenges
+                = challengeUserRepository.findAllByUser(user, PageRequest.of(1, 2));
+
+        //then
+        Assertions.assertThat(participatingChallenges.size()).isEqualTo(1);
+        Assertions.assertThat(participatingChallenges.get(0).getIsAutomated()).isEqualTo(true);
+    }
+}
