05d378eb0164c97cf687446b8e4bdddcd92e19fb
SENUNGHOONYANG <sheepsh93@gmail.com>
2022-01-25 10:32:32 +0900

    feat : Create dtos related to savings

---
+++ src/main/java/com/savle/togethersaving/dto/user/CreateSavingsDto.java
@@ -0,0 +1,17 @@
+package com.savle.togethersaving.dto.user;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Builder
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class CreateSavingsDto {
+
+    private Long challengePayment;
+    private String physicalAccountNumber;
+    private String cmaAccountNumber;
+}
