ab03d2120b3154465955e74b0d5b57b1fabb4e25
SENUNGHOONYANG <sheepsh93@gmail.com>
2022-01-27 15:53:51 +0900

    feat : ChallengeUser 객체 초기화 method

--- src/main/java/com/savle/togethersaving/entity/ChallengeUser.java
+++ src/main/java/com/savle/togethersaving/entity/ChallengeUser.java
@@ -7,7 +7,6 @@ import org.hibernate.annotations.DynamicInsert;
 import javax.persistence.*;
 
 
-@AllArgsConstructor
 @NoArgsConstructor
 @Builder
 @Entity
@@ -33,6 +32,15 @@ public class ChallengeUser {
     @JoinColumn(name = "user_id", nullable = false, foreignKey = @ForeignKey(ConstraintMode.CONSTRAINT))
     @MapsId("userId")
     private User user;
+
+    public ChallengeUser(ChallengeUserPK challengeUserPK, Long accumulatedBalance, Boolean isAutomated
+            , Challenge challenge, User user) {
+        this.challengeUserPK = new ChallengeUserPK();
+        this.accumulatedBalance = 0L;
+        this.isAutomated = false;
+        this.challenge = challenge;
+        this.user = user;
+    }
 
     public void addBalance(Long amount){
         this.accumulatedBalance += amount;
