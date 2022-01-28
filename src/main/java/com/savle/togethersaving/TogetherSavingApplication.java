562c99c20367c2d9c58ac52126c8382067a1340a
SENUNGHOONYANG <sheepsh93@gmail.com>
2022-01-28 22:58:18 +0900

    feat : Create BCryptPasswordEncoder

--- src/main/java/com/savle/togethersaving/TogetherSavingApplication.java
+++ src/main/java/com/savle/togethersaving/TogetherSavingApplication.java
@@ -2,11 +2,19 @@ package com.savle.togethersaving;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @EnableAspectJAutoProxy
 @SpringBootApplication
 public class TogetherSavingApplication {
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(TogetherSavingApplication.class, args);
     }
