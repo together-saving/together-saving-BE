0eb6621504fd5a483547eeb8773717e317b4d0e5
Jeongisu94 <mustdoit2020@gmail.com>
2022-01-28 14:46:33 +0900

    feat : configure cors

--- src/main/java/com/savle/togethersaving/TogetherSavingApplication.java
+++ src/main/java/com/savle/togethersaving/TogetherSavingApplication.java
@@ -2,13 +2,26 @@ package com.savle.togethersaving;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @EnableAspectJAutoProxy
 @SpringBootApplication
 public class TogetherSavingApplication {
     public static void main(String[] args) {
         SpringApplication.run(TogetherSavingApplication.class, args);
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("*");
+            }
+        };
     }
 }
 
