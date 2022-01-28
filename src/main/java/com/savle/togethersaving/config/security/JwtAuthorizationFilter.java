d361d49e669052a6b895b2228320d4260886a702
SENUNGHOONYANG <sheepsh93@gmail.com>
2022-01-28 22:51:55 +0900

    feat : Create authentication filter and authorization filter

---
+++ src/main/java/com/savle/togethersaving/config/security/JwtAuthorizationFilter.java
@@ -0,0 +1,74 @@
+package com.savle.togethersaving.config.security;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.savle.togethersaving.entity.User;
+import com.savle.togethersaving.repository.UserRepository;
+import com.savle.togethersaving.secret.JwtProperties;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+
+@Slf4j
+public class JwtAuthorizationFilter extends BasicAuthenticationFilter {
+
+
+    private UserRepository userRepository;
+
+    public JwtAuthorizationFilter(AuthenticationManager authenticationManager, UserRepository userRepository) {
+        super(authenticationManager);
+        this.userRepository = userRepository;
+    }
+
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response
+            , FilterChain chain) throws IOException, ServletException {
+
+        String header = request.getHeader(JwtProperties.HEADER_STRING);
+        if (header == null || !header.startsWith(JwtProperties.TOKEN_PREFIX)) {
+            chain.doFilter(request, response);
+            return;
+        }
+
+        String token = request.getHeader(JwtProperties.HEADER_STRING)
+                .replace(JwtProperties.TOKEN_PREFIX, "");
+
+
+        String username = JWT.require(Algorithm.HMAC512(JwtProperties.SECRET))
+                .build()
+                .verify(token)
+                .getClaim("email")
+                .asString();
+
+
+        if (username != null) {
+            User user = userRepository.findByEmail(username);
+
+            CustomUserDetails customUserDetails = new CustomUserDetails(user);
+
+            Authentication authentication =
+                    new UsernamePasswordAuthenticationToken(
+                            customUserDetails,
+                            null,
+                            customUserDetails.getAuthorities());
+
+
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+            chain.doFilter(request, response);
+        }
+
+    }
+
+
+}
