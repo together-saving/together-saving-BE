7ae6c1fdda7737ac26c6e529d6024ced618b0c66
SENUNGHOONYANG <sheepsh93@gmail.com>
2022-02-03 13:34:12 +0900

    refactor : Spring security 사용 후 컨트롤러 변경

--- src/main/java/com/savle/togethersaving/controller/ChallengeController.java
+++ src/main/java/com/savle/togethersaving/controller/ChallengeController.java
@@ -1,18 +1,17 @@
 package com.savle.togethersaving.controller;
 
+import com.savle.togethersaving.config.security.CustomUserDetails;
 import com.savle.togethersaving.dto.Data;
-import com.savle.togethersaving.repository.ChallengeRepository;
-import com.savle.togethersaving.repository.UserRepository;
 import com.savle.togethersaving.service.ChallengeService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
-import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 @Slf4j
@@ -28,10 +27,10 @@ public class ChallengeController {
                                               @RequestParam String criteria,@RequestParam int page) {
 
         PageRequest pageable = null;
-        if(criteria.equals("popularity")){
-            pageable = PageRequest.of(page,7,Sort.by("members").descending());
-        }else if(criteria.equals("valid")){
-            pageable = PageRequest.of(page,7,Sort.by("challenge.members").descending());
+        if (criteria.equals("popularity")) {
+            pageable = PageRequest.of(page, 7, Sort.by("members").descending());
+        } else if (criteria.equals("valid")) {
+            pageable = PageRequest.of(page, 7, Sort.by("challenge.members").descending());
         }
 
 
@@ -39,16 +38,22 @@ public class ChallengeController {
     }
 
     @PostMapping("/challenges/{challengeId}/payment")
-    public HttpEntity<?> changeAutoSetting(@PathVariable Long challengeId) {
-        Long userId = 1L;
+    public HttpEntity<?> payChallenge(@PathVariable Long challengeId, Authentication auth) {
+
+
+        CustomUserDetails customDetails = (CustomUserDetails) auth.getPrincipal();
+        Long userId = customDetails.getUser().getUserId();
         challengeService.payForChallenge(userId, challengeId);
 
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
     @PostMapping("/challenges/{challengeId}/auto")
-    public HttpEntity<?> modifyAutoSetting(@PathVariable Long challengeId) {
-        Long userId = 1L;
+    public HttpEntity<?> modifyAutoSetting(@PathVariable Long challengeId, Authentication auth) {
+
+        CustomUserDetails customDetails = (CustomUserDetails) auth.getPrincipal();
+        Long userId = customDetails.getUser().getUserId();
+
         challengeService.changeAutoSettings(userId, challengeId);
 
         return new ResponseEntity<>(HttpStatus.OK);
