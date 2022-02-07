48164a5a779dd340a5f4c8d7d1465e9fa483caae
SENUNGHOONYANG <sheepsh93@gmail.com>
2022-02-07 20:37:49 +0900

    feat : Comment, challenge,user 양방향 맵핑 List 생성

--- src/main/java/com/savle/togethersaving/entity/ChallengeComment.java
+++ src/main/java/com/savle/togethersaving/entity/ChallengeComment.java
@@ -18,12 +18,32 @@ public class ChallengeComment extends BaseTime {
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id", nullable = false, foreignKey = @ForeignKey(ConstraintMode.CONSTRAINT))
-    private User user;
+    private User writer;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "challenge_id", nullable = false, foreignKey = @ForeignKey(ConstraintMode.CONSTRAINT))
-    private Challenge Challenge;
+    private Challenge challenge;
 
     private String content;
+
+    public static ChallengeComment createComment(User writer, Challenge challenge,String content) {
+
+        return ChallengeComment.builder()
+                .writer(writer)
+                .challenge(challenge)
+                .content(content)
+                .build();
+
+    }
+
+    public void changeCommentListOfUser(User user){
+        this.writer = user;
+        this.writer.getCommentList().add(this);
+    }
+
+    public void changeCommentListOfChallenge(Challenge challenge){
+        this.challenge = challenge;
+        this.challenge.getCommentList().add(this);
+    }
 
 }
