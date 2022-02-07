a3de0e370a566789e9aa2be176e071dcc83aafb4
Jeongisu94 <mustdoit2020@gmail.com>
2022-02-07 20:14:05 +0900

    feat : implement saving ranking api

--- src/main/java/com/savle/togethersaving/repository/ChallengeUserRepository.java
+++ src/main/java/com/savle/togethersaving/repository/ChallengeUserRepository.java
@@ -1,20 +1,26 @@
 package com.savle.togethersaving.repository;
 
+import com.savle.togethersaving.entity.Challenge;
 import com.savle.togethersaving.entity.ChallengeUser;
 import com.savle.togethersaving.entity.ChallengeUserPK;
 import com.savle.togethersaving.entity.User;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
-@Repository
 public interface ChallengeUserRepository extends JpaRepository<ChallengeUser, ChallengeUserPK> {
 
 
     List<ChallengeUser> findAllByUser_UserId(Long userId, Pageable pageable);
-
     ChallengeUser findByChallengeUserPK_ChallengeIdAndChallengeUserPK_UserId(Long challengeId, Long userId);
+
+    List<ChallengeUser> findAllByChallenge_ChallengeId (Long ChallengeId);
+
     boolean existsByChallengeUserPK_ChallengeIdAndChallengeUserPK_UserId(Long challengeId, Long userId);
 }
--- src/main/java/com/savle/togethersaving/service/SavingService.java
+++ src/main/java/com/savle/togethersaving/service/SavingService.java
@@ -1,6 +1,7 @@
 package com.savle.togethersaving.service;
 
 import com.savle.togethersaving.dto.saving.SavingDetailDto;
+import com.savle.togethersaving.dto.saving.SavingRankingDto;
 import com.savle.togethersaving.dto.saving.SavingStatusDto;
 import com.savle.togethersaving.entity.*;
 import com.savle.togethersaving.repository.*;
@@ -12,8 +13,11 @@ import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
 import java.time.temporal.ChronoUnit;
+import java.util.Comparator;
 import java.util.List;
+import java.util.function.BiConsumer;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @Service
 @RequiredArgsConstructor
@@ -49,8 +53,6 @@ public class SavingService {
         return SavingStatusDto.of(account,challengeUser,transactionLogs);
     }
 
-
-
     public SavingDetailDto getSavingDetail(Long userId, Long challengeId) {
         User user = userRepository.getUserByUserId(userId);
         ChallengeUser challengeUser = challengeUserRepository.findByChallengeUserPK_ChallengeIdAndChallengeUserPK_UserId(challengeId, userId);
@@ -58,7 +60,7 @@ public class SavingService {
         Challenge challenge = challengeRepository.getByChallengeId(challengeId);
 
         Integer savingRate =
-                Math.toIntExact(challengeUser.getAccumulatedBalance()*100 / (challengeCount.getMaxCount() * challenge.getPayment()) ) ;
+                calculateSavingRatio(Math.toIntExact(challengeUser.getAccumulatedBalance()),challengeCount.getMaxCount(),Math.toIntExact(challenge.getPayment()));
 
         Integer successCount =
                 transactionLogRepository.getSuccessCount(userId, challengeId);
@@ -72,5 +74,21 @@ public class SavingService {
                 .failureCount(challengeCount.getCurrentCount() - successCount)
                 .thumbnail(user.getProfilePicture())
         .build();
+    }
+
+    public List<SavingRankingDto> getSavingRanking(Long challengeId) {
+        List<ChallengeUser> challengeUsers = challengeUserRepository.findAllByChallenge_ChallengeId(challengeId);
+        Integer maxCount = challengeCountRepository.getChallengeCountByChallengeId(challengeId).getMaxCount();
+        Integer challengePayment = Math.toIntExact(challengeRepository.getByChallengeId(challengeId).getPayment());
+
+        return challengeUsers.stream().map(challengeUser -> {
+            SavingRankingDto savingRankingDto = SavingRankingDto.userFrom(challengeUser.getUser());
+            savingRankingDto.setSavingRate( calculateSavingRatio(Math.toIntExact(challengeUser.getAccumulatedBalance()),maxCount,challengePayment) );
+            return savingRankingDto;
+        }).sorted(Comparator.comparing(SavingRankingDto::getSavingRate).reversed()).collect(Collectors.toList());
+    }
+
+    private Integer calculateSavingRatio(Integer accumulatedBalance, Integer maxCount, Integer payment) {
+        return  (( (accumulatedBalance*100) / ( maxCount * payment )));
     }
 }
