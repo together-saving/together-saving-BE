6893745b3ee208f12ea891791e883c0dc0709632
SENUNGHOONYANG <sheepsh93@gmail.com>
2022-02-11 12:46:27 +0900

    test : ChallengeUserRepositoryTest, CommentRepositoryTest 리팩토링

--- src/test/java/com/savle/togethersaving/repository/CommentRepositoryTest.java
+++ src/test/java/com/savle/togethersaving/repository/CommentRepositoryTest.java
@@ -1,28 +1,43 @@
 package com.savle.togethersaving.repository;
 
-import com.savle.togethersaving.entity.ChallengeComment;
+import com.savle.togethersaving.entity.Challenge;
+import com.savle.togethersaving.entity.Comment;
+import com.savle.togethersaving.entity.User;
+import com.savle.togethersaving.repository.repositoryfixture.CommentFixture;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static com.savle.togethersaving.repository.repositoryfixture.ChallengeFixture.createChallenge;
+import static com.savle.togethersaving.repository.repositoryfixture.CommentFixture.createComment;
+import static com.savle.togethersaving.repository.repositoryfixture.UserFixture.createUser;
 
-class CommentRepositoryTest extends RepositoryTestUtil{
+@DataJpaTest
+class CommentRepositoryTest{
+
+    @Autowired
+    private UserRepository userRepository;
+    @Autowired
+    private ChallengeRepository challengeRepository;
+    @Autowired
+    private CommentRepository commentRepository;
 
     @Test
     void findComment() {
-        createUserAndChallengeSaved();
-        ChallengeComment comment = ChallengeComment.builder()
-                .challenge(previousChallenge)
-                .writer(user)
-                .content("test").build();
-        comment.setCreatedAt(LocalDateTime.now());
-        commentRepository.save(comment);
-        List<ChallengeComment> comments = commentRepository.findCommentFrom(previousChallenge.getChallengeId(), 0);
-        Assertions.assertThat(comments.size()).isEqualTo(1);
-        Assertions.assertThat(comments.get(0).getWriter().getUserId()).isEqualTo(user.getUserId());
+        // given
+        User savedUser = userRepository.save(createUser());
+        Challenge savedChallenge = challengeRepository.save(createChallenge(savedUser));
+        commentRepository.saveAll(createComment(savedUser,savedChallenge));
+
+        //when
+        List<Comment> comments = commentRepository.findCommentFrom(savedChallenge.getChallengeId());
+
+        //then
+        Assertions.assertThat(comments.size()).isEqualTo(10);
+        Assertions.assertThat(comments.get(0).getWriter().getUserId()).isEqualTo(savedUser.getUserId());
     }
 }
