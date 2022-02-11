85ae841fca13edf79897685a957502be3991a79d
SENUNGHOONYANG <sheepsh93@gmail.com>
2022-02-11 16:54:33 +0900

    refacor : db수정

--- src/main/java/com/savle/togethersaving/config/security/SecurityConfig.java
+++ src/main/java/com/savle/togethersaving/config/security/SecurityConfig.java
@@ -43,7 +43,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .addFilter(new JwtAuthenticationFilter(authenticationManager()))
                 .addFilter(new JwtAuthorizationFilter(authenticationManager(), userRepository))
                 .authorizeRequests()
-                .antMatchers(HttpMethod.OPTIONS, "/api/**").permitAll() //OPTIONS 메소드 허락
+                .antMatchers(HttpMethod.OPTIONS, "/api/**").permitAll()
                 .antMatchers("/api/v1/users/**")
                 .hasRole("USER")
                 .antMatchers("/api/v1/users/reviews/**")
