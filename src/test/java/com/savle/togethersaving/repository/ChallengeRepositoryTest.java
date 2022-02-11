627feff18d19a2e2fc7ff1c5b4c2a53e3e01a5ae
SENUNGHOONYANG <sheepsh93@gmail.com>
2022-02-11 12:47:03 +0900

    test : ChallengeRepositoryTest, ChallengeTagRepositoryTest 리팩토링

--- src/test/java/com/savle/togethersaving/repository/ChallengeRepositoryTest.java
+++ src/test/java/com/savle/togethersaving/repository/ChallengeRepositoryTest.java
@@ -1,32 +1,43 @@
 package com.savle.togethersaving.repository;
 
 import com.savle.togethersaving.entity.Challenge;
+import com.savle.togethersaving.entity.User;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
 
 import java.time.LocalDate;
 import java.util.List;
 
+import static com.savle.togethersaving.repository.repositoryfixture.ChallengeFixture.createChallengeList;
+import static com.savle.togethersaving.repository.repositoryfixture.UserFixture.createUser;
 
-class ChallengeRepositoryTest extends RepositoryTestUtil {
 
+@DataJpaTest
+class ChallengeRepositoryTest {
+
+    @Autowired
+    private UserRepository userRepository;
+    @Autowired
+    private ChallengeRepository challengeRepository;
 
     @Test
     @DisplayName("아직 시작하지 않은 모든 챌린지들을 조회한다. 성공하는 케이스")
     void beforeStartChallengeTest() {
         //given
-        int beforeSize = challengeRepository.findChallengesByStartDateGreaterThan(LocalDate.now()).size();
-        createUserAndChallengeSaved();
+        User savedUser = userRepository.save(createUser());
+        challengeRepository.saveAll(createChallengeList(savedUser));
 
         //when
-        List<Challenge> challengesByStartDateAfter
+        List<Challenge> findChallenges
                 = challengeRepository.findChallengesByStartDateGreaterThan(LocalDate.now());
 
         //then
-        Assertions.assertThat(challengesByStartDateAfter.size()).isEqualTo(beforeSize+8);
+        Assertions.assertThat(findChallenges.size()).isEqualTo(10);
 
     }
 
@@ -34,17 +45,14 @@ class ChallengeRepositoryTest extends RepositoryTestUtil {
     @DisplayName("인기있는 모든 챌린지들을 조회한다. 성공하는 케이스")
     void popularChallengeTest() {
         //given
-        createUserAndChallengeSaved();
-
-        challengeRepository.save(previousChallenge);
-        challengeRepository.save(afterChallenge);
-        challengeRepository.save(biggest);
+        User savedUser = userRepository.save(createUser());
+        challengeRepository.saveAll(createChallengeList(savedUser));
 
         //when
         List<Challenge> popularChallenges
                 = challengeRepository
-                .findChallengesByStartDateGreaterThan(LocalDate.now(), PageRequest.of(1, 7, Sort.by("members").descending()));
+                .findChallengesByStartDateGreaterThan(LocalDate.now(), PageRequest.of(0, 7, Sort.by("members").descending()));
         //then
-        Assertions.assertThat(popularChallenges.get(0).getMembers()).isEqualTo(13L);
+        Assertions.assertThat(popularChallenges.get(0).getMembers()).isEqualTo(10L);
     }
 }
--- src/test/java/com/savle/togethersaving/repository/ChallengeTagRepositoryTest.java
+++ src/test/java/com/savle/togethersaving/repository/ChallengeTagRepositoryTest.java
@@ -1,22 +1,45 @@
 package com.savle.togethersaving.repository;
 
+import com.savle.togethersaving.entity.Challenge;
 import com.savle.togethersaving.entity.ChallengeTag;
+import com.savle.togethersaving.entity.Tag;
+import com.savle.togethersaving.entity.User;
+import com.savle.togethersaving.repository.repositoryfixture.ChallengeTagFixture;
+import com.savle.togethersaving.repository.repositoryfixture.TagFixture;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 import java.util.List;
 
+import static com.savle.togethersaving.repository.repositoryfixture.ChallengeFixture.createChallenge;
+import static com.savle.togethersaving.repository.repositoryfixture.ChallengeTagFixture.createChallengeTag;
+import static com.savle.togethersaving.repository.repositoryfixture.UserFixture.createUser;
 
-class ChallengeTagRepositoryTest extends RepositoryTestUtil {
+
+@DataJpaTest
+class ChallengeTagRepositoryTest {
+
+    @Autowired
+    private UserRepository userRepository;
+    @Autowired
+    private ChallengeRepository challengeRepository;
+    @Autowired
+    private TagRepository tagRepository;
+    @Autowired
+    private ChallengeTagRepository challengeTagRepository;
+
 
     @Test
     void findChallengeTagsByChallenge() {
         //given
-        createUserAndChallengeSaved();
-        createTagsAndChallengeTag();
-
+        User savedUser = userRepository.save(createUser());
+        Challenge savedChallenge = challengeRepository.save(createChallenge(savedUser));
+        List<Tag> savedTags = tagRepository.saveAll(TagFixture.createTagsAndChallengeTag());
+        challengeTagRepository.saveAll(createChallengeTag(savedChallenge,savedTags));
         //when
-        List<ChallengeTag> tagList = challengeTagRepository.findChallengeTagsByChallenge(previousChallenge);
+        List<ChallengeTag> tagList = challengeTagRepository.findChallengeTagsByChallenge(savedChallenge);
         //then
         Assertions.assertThat(tagList.get(0).getTag().getName()).isEqualTo("first tag!");
         Assertions.assertThat(tagList.get(1).getTag().getName()).isEqualTo("second tag!");
